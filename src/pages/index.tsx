bd5731d9feab58340058ccafaf3f3323d7ef0387
walteralves10 <walter.rezende@simpleagro.com.br>
2024-09-27 16:05:20 -0300

    feat: initial structure project in next

---
+++ src/pages/index.tsx
@@ -0,0 +1,89 @@
+import Image from "next/image";
+import { HomeContainer, Product } from "../styles/pages/home";
+import Head from "next/head";
+import 'keen-slider/keen-slider.min.css'
+import { useKeenSlider } from 'keen-slider/react'
+import { stripe } from "../lib/stripe";
+import { GetStaticProps } from "next";
+import Stripe from "stripe";
+import Link from "next/link";
+
+interface HomeProps {
+  products: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: string;
+  }[]
+}
+
+export default function Home({ products }: HomeProps) {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    }
+  })
+
+  return (
+    <>
+    <Head>
+      <title>Home | Ignite Shop</title>
+    </Head>
+    
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      {products?.map(product => {
+        return (
+          <Link
+            href={`/product/${product.id}`}
+            key={product.id}
+            prefetch={false}
+          >
+            <Product
+              className="keen-slider__slide"
+            >
+              <Image src={product.imageUrl} width={520} height={480} alt="" />
+
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
+        )
+      })}
+    </HomeContainer>
+    </>
+  )
+}
+
+// file-system Routing
+// Roteamento baseado em arquivos físicos
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await stripe.products.list({
+    expand: ['data.default_price']
+  })
+
+  const products = response.data.map(product => {
+    const price = product.default_price as Stripe.Price
+
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      url: product.url,
+      price: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+      }).format((price?.unit_amount ?? 0) / 100),
+    }
+  })
+  
+  return {
+    props: {
+      products,
+    },
+    revalidate: 60 * 60 * 2, // 2 hours
+  }
+}
